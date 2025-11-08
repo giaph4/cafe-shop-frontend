ed17635f95a366efd8dea90581b724930ae7752c
Ɠia Pɦo Huỳnh <huynhgiapho1234@gmail.com>
2025-11-08 21:50:46 +0700

    fix heigh image

--- src/views/POS.vue
+++ src/views/POS.vue
@@ -587,7 +587,7 @@ watch(() => posStore.isModalOpen, (newValue, oldValue) => {
 
 .product-image {
   width: 100%;
-  height: 140px;
+  max-height: 140px;
   border-radius: 8px;
   overflow: hidden;
   display: block;
